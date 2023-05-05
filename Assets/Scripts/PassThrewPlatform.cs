2b03e0f137d8939684d323a2c3b2a3c2621943a0
Andrej Korica <koricaandrej3@gmail.com>
2023-05-05 14:33:15 +0200

    particle effects, physics on tilemap, spike hitboxes
    platform animations don't work

---
+++ Assets/Scripts/PassThrewPlatform.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PassThrewPlatform : MonoBehaviour
+{
+    private Collider2D _collider;
+    private bool _playerOnPlatform;
+
+    private void Start()
+    {
+        _collider = GetComponent<Collider2D>();
+    }
+
+    private void Update()
+    {
+        if (_playerOnPlatform && Input.GetAxisRaw("Vertical") < 0)
+        {
+        
+            _collider.enabled = false;
+            StartCoroutine(EnableCollider()); 
+        }
+    }
+
+    private IEnumerator EnableCollider()
+    {
+        yield return new WaitForSeconds(0.5f);
+        _collider.enabled = true;
+    }
+    /*
+    private void SetPlayerOnPLatform(Collision2D other, bool value)
+    {
+        var player = other.gameObject.GetComponent<Player>();
+        if (player != null)
+        {
+            _playerOnPlatform = value;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        SetPlayerOnPLatform(true); 
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        SetPlayerOnPLatform(false);
+    }
+    */
+}
