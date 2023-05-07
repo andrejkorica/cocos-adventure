a6c3e1cd9646646fe20977c16928f19cd3e64213
Rafael Krstačić <35665918+rkrstacic@users.noreply.github.com>
2023-05-07 02:29:41 +0200

    Dodan pause screen i timer

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    public int MainMenuIndex = 0;
+    public GameObject MainMenuPanel;
+    public GameObject HUDPanel;
+
+    public void Start() {
+        Time.timeScale = 1;
+        MainMenuPanel.SetActive(false);
+        HUDPanel.SetActive(true);
+
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            Time.timeScale = MainMenuPanel.activeSelf ? 1 : 0;
+            MainMenuPanel.SetActive(!MainMenuPanel.activeSelf);
+            HUDPanel.SetActive(!HUDPanel.activeSelf);
+        }
+    }
+
+    public void Continue() {
+        Time.timeScale = 1;
+        MainMenuPanel.SetActive(false);
+        HUDPanel.SetActive(true);
+    }
+
+    public void Exit() {
+        Time.timeScale = 1;
+
+        // TODO: Treba unloadat scenu da ne ostane spremljen state
+        SceneManager.UnloadSceneAsync(gameObject.scene.buildIndex);
+        SceneManager.LoadScene(MainMenuIndex);
+    }
+}
