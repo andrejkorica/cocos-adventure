6c4dc4bbfd3b98005d66a7d84ada9be58456a926
Rafael Krstačić <35665918+rkrstacic@users.noreply.github.com>
2023-05-28 18:15:36 +0200

    Fix

--- Assets/Scripts/CollectingKeyAndChest.cs
+++ Assets/Scripts/CollectingKeyAndChest.cs
@@ -3,7 +3,7 @@ using UnityEngine.SceneManagement;
 
 public class CollectingKeyAndChest : MonoBehaviour
 {
-    private bool hasKey = true; 
+    private bool hasKey = false; 
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
