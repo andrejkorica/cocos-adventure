bb7e008478206c66fd1b72defa7ca61b18d6280b
Andrej Korica <koricaandrej3@gmail.com>
2023-06-11 14:24:13 +0200

    starfish fix

--- Assets/Scripts/StarScript.cs
+++ Assets/Scripts/StarScript.cs
@@ -14,7 +14,7 @@ public class StarScript : MonoBehaviour, IDataPersistence
     
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (!collision.gameObject.CompareTag("Player") && !isCollected)
+        if (collision.gameObject.CompareTag("Player") && !isCollected)
         {
             isCollected = true;
             spriteRenderer.enabled = false;
