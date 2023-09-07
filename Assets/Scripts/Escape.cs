307379070244e957403273246275d758d24cd70f
stela88 <49338331+stela88@users.noreply.github.com>
2023-09-07 15:48:25 +0200

    End scene fix

---
+++ Assets/Scripts/Escape.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Escape : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+
+            SceneManager.LoadScene("Main menu");
+        }
+    }
+}
