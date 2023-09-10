5f949fa7101efc1ad220cfebcbeb6252a2d12211
Andrej Korica <koricaandrej3@gmail.com>
2023-09-11 01:17:09 +0200

    fixed movment animation

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -12,16 +12,19 @@ public class PlayerMovement : MonoBehaviour
     private CharacterController2D Controller;
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask _groundLayer;
+    private Animator _animator;
 
     public void Start()
     {
         Controller = GetComponent<CharacterController2D>();
         rb = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
     }
 
     public void Update()
     {
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        _animator.SetBool("isRunning", horizontalMove != 0);
 
         if (Input.GetButtonDown("Jump") && Controller.IsGrounded() && IsGrounded())
         {
@@ -38,4 +41,3 @@ public class PlayerMovement : MonoBehaviour
         return Physics2D.OverlapCircle(groundCheck.position, 0.3f, _groundLayer);
     }
 }
-
